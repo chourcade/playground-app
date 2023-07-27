d2949aca3c2100bfc2265675c85c332b4ba7114b
Cristian Hourcade <chourcade@Cristians-MacBook-Pro.local>
2023-07-27 15:53:09 -0300

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { NextFederationPlugin } = require('@module-federation/nextjs-mf');
 
-module.exports = nextConfig
+ const nextConfig = {
+   reactStrictMode: true,
+   webpack(config, { isServer }) {
+     config.plugins.push(
+       new NextFederationPlugin({
+         name: 'playground',
+         filename: 'static/chunks/remoteEntry.js',
+         exposes: {
+           // specify exposed pages and components
+           './Playground': './src/pages/playground.tsx',
+         },
+         shared: {
+            // specify shared dependencies 
+            // read more in Shared Dependencies section
+         },
+       })
+     );
+
+     return config;
+   },
+ }
+ 
+ module.exports = nextConfig
---
+++ src/pages/playground.tsx
@@ -0,0 +1,7 @@
+export default function LoginPage () {
+    return (
+        <div>
+            <h2>Hello World, im and external app ;) </h2>
+        </div>
+    )
+}
